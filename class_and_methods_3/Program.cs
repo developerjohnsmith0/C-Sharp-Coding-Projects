c0028f34bd4d2b83b29354f78a7e9ecaa02961d8
developerjohnsmith0 <87449450+developerjohnsmith0@users.noreply.github.com>
2022-02-24 13:50:52 -0500

    class and method3

---
+++ class_and_methods_3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace class_and_methods_3
+{
+    public class OperationNumber
+    {
+        public void functionNumber(int num1, int num2)
+        {
+            var Num = num1 + 5;
+            Console.WriteLine(num2);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            OperationNumber operationNumber = new OperationNumber();
+            int int1 = 5;
+            int int2 = 5;
+            operationNumber.functionNumber(int1, int2);
+        }
+    }
+}
