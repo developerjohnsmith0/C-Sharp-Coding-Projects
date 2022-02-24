d0e89ea252a343cd0fea067292f77d2f5c061f1f
developerjohnsmith0 <87449450+developerjohnsmith0@users.noreply.github.com>
2022-02-24 13:49:58 -0500

    class and method 2

---
+++ class_and_methods_2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Collections.Generic;
+
+namespace class_and_methods_2
+{
+    public class OptionalParameters
+    {
+        public void addNum(int number1, [Optional] int number2)
+        {
+            Console.WriteLine(number1 + 5);
+            Console.WriteLine(number2 + 5);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            OptionalParameters operations = new OptionalParameters();
+            Console.WriteLine("Enter first number:");
+            int inputnumber1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter second number:");
+            int inputnumber2 = Convert.ToInt32(Console.ReadLine());
+            operations.addNum(inputnumber1, inputnumber2);
+            Console.WriteLine("No need to add second paramenter tou can also run it by passing one parameter.");
+        }
+    }
+}
