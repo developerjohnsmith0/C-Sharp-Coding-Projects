7e12b56463314d3d9d1b3045f5fe7c1b962e73ad
developerjohnsmith0 <87449450+developerjohnsmith0@users.noreply.github.com>
2022-02-24 13:53:21 -0500

    inheritance assignment

---
+++ inheritence/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace inheritence
+{
+    public class Person
+    {
+        public String FirstName;
+        public String LastName;
+        public void SayName()
+        {
+            Console.WriteLine("Name: " + "[ " + FirstName + " " + LastName + " ]");
+        }
+    }
+
+    public class Employee : Person
+    {
+        int Id;
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee();
+            employee.FirstName = "Sample";
+            employee.LastName = "Student";
+            employee.SayName();
+        }
+    }
+}
