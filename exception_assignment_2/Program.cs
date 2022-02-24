6184b6506a4e077f35375d1e1147347e4ff53fa4
developerjohnsmith0 <87449450+developerjohnsmith0@users.noreply.github.com>
2022-02-24 13:47:41 -0500

    exception part2

---
+++ exception_assignment_2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace more_exception
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Please enter your age:");
+                uint ageOfUser = Convert.ToUInt32(Console.ReadLine());
+                uint presentYear = Convert.ToUInt32(System.DateTime.Now.Year);
+                uint yearOfBirth = presentYear - ageOfUser;
+                Console.WriteLine(yearOfBirth + " is the year you were born in.");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("The age entered is invalid.");
+            }
+        }
+    }
+}
