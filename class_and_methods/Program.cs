30d67670e1ecf34c54e22cef78910694e71720c3
developerjohnsmith0 <87449450+developerjohnsmith0@users.noreply.github.com>
2022-02-24 13:52:06 -0500

    class and method

---
+++ class_and_methods/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace class_and_method
+{
+    public class Operation
+    {
+        public void Divide(int num)
+        {
+            Console.WriteLine(num / 2);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Operation division = new Operation();
+            Console.WriteLine("enter number to divide it by 2:");
+            int enteredNumber = Convert.ToInt32(Console.ReadLine());
+            division.Divide(enteredNumber);
+        }
+    }
+}
