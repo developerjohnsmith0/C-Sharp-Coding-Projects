cc01087f546bdd7495206764e1b3b71b3772ed01
developerjohnsmith0 <87449450+developerjohnsmith0@users.noreply.github.com>
2022-02-24 16:04:09 -0500

    date and time assignment

---
+++ date_and_time/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace date_and_time
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DateTime.Now);
+            int enteredNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(enteredNumber + " " + DateTime.Now.ToLongTimeString());
+        }
+    }
+}
