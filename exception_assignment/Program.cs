826fe4e399c43f25a44fd4e5e01f77a04624649a
developerjohnsmith0 <87449450+developerjohnsmith0@users.noreply.github.com>
2022-02-24 16:06:31 -0500

    exception assignment

---
+++ exception_assignment/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace exception_assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                List<int> listOfIntegers = new List<int>() { 5, 10, 15, 20, 25 };
+                Console.WriteLine("Enter number to divide.");
+                int enteredNumber = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Result:");
+
+                foreach (int num in listOfIntegers)
+                {
+                    Console.WriteLine(num / enteredNumber);
+                }
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.ReadKey();
+            }
+        }
+    }
+}
