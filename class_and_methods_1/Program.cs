d8f207664bcc4aeefe323bbc8b25af261ce0dafa
developerjohnsmith0 <87449450+developerjohnsmith0@users.noreply.github.com>
2022-02-24 13:48:58 -0500

    class and method1

---
+++ class_and_methods_1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace class_and_methods_1
+{
+    public class operationOfInt
+    {
+        public void operation(int number)
+        {
+            Console.WriteLine(number + 5);
+        }
+
+        public void operation(double number)
+        {
+            Console.WriteLine(number + number);
+        }
+
+        public void operation(string number)
+        {
+            var newNum = Convert.ToInt32(number);
+            Console.WriteLine(newNum * newNum);
+        }
+    }
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            operationOfInt operate = new operationOfInt();
+            operate.operation(5);
+            operate.operation(5.5);
+            operate.operation("5");
+        }
+    }
+}
