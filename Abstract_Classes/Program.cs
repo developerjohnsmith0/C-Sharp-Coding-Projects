0f6721cd917ce05cf8c243da61a9ad5305adfeaf
developerjohnsmith0 <87449450+developerjohnsmith0@users.noreply.github.com>
2022-02-24 13:54:25 -0500

    abstract class assignment

---
+++ Abstract_Classes/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Abstract_Classes
+{
+    public abstract class Person
+    {
+        public string firstName;
+        public string lastName;
+
+        public abstract void SayName();
+    }
+
+    public class Employee : Person
+    {
+        public override void SayName()
+        {
+            Console.WriteLine(firstName + " " + lastName);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee();
+            employee.firstName = "Sample";
+            employee.lastName = "Student";
+            employee.SayName();
+        }
+    }
+}
