22c7b32905e862a95bbeb962c3eb8b062c4eba55
developerjohnsmith0 <87449450+developerjohnsmith0@users.noreply.github.com>
2022-02-24 16:07:35 -0500

    additional features assignment

---
+++ Additional_Features/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Additional_Features
+{
+    public class MyClass
+    {
+        public string filmName;
+        public const string film = "Tenet";
+        public MyClass() : this(film) { }
+
+        public MyClass(string newFlimName)
+        {
+            var newName = newFlimName;
+            filmName = newName;
+        }
+        static void Main(string[] args)
+        {
+            MyClass myClass = new MyClass();
+            Console.WriteLine(myClass.filmName);
+        }
+    }
+}
