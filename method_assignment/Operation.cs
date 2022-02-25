7c1b133f64726dd4afdf5c443c210982e61d0a78
developerjohnsmith0 <87449450+developerjohnsmith0@users.noreply.github.com>
2022-02-25 09:47:18 -0500

    method assignment

---
+++ method_assignment/Operation.cs
@@ -0,0 +1,22 @@
+using System;
+
+namespace method_assignment
+{
+    public class MethodOperation
+    {
+        public void Addition(int num)
+        {
+            Console.WriteLine(num + 5);
+
+        }
+        public void Substraction(int num)
+        {
+            Console.WriteLine(num - 5);
+
+        }
+        public void Division(int num)
+        {
+            Console.WriteLine(num / 5);
+        }
+    }
+}
---
+++ method_assignment/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace method_assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MethodOperation methodOperation = new MethodOperation();
+
+            Console.WriteLine("Enter number:");
+            int enteredNum = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("On Adding we get");
+            methodOperation.Addition(enteredNum);
+
+            Console.WriteLine("On Subtracting we get");
+            methodOperation.Substraction(enteredNum);
+
+            Console.WriteLine("On Mutiplying we get");
+            methodOperation.Division(enteredNum);
+        }
+    }
+}
