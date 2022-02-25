de59c5c5f968486da864d8df25be5883350ac953
developerjohnsmith0 <87449450+developerjohnsmith0@users.noreply.github.com>
2022-02-25 09:45:26 -0500

    interface assignment

---
+++ interface_assignment/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace interfaces_assingment
+{
+    interface IQuitable
+    {
+        void Quit();
+    }
+    class Employee : IQuitable
+    {
+        public void Quit()
+        {
+            Console.WriteLine("Lorem ipsum doler sit.");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee();
+            employee.Quit();
+        }
+    }
+}
