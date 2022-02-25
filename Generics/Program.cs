f2f94f4d4ff03baa53732c2287d68a33c85f2fa0
developerjohnsmith0 <87449450+developerjohnsmith0@users.noreply.github.com>
2022-02-25 09:44:09 -0500

    generic assignment

---
+++ Generics/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Generics
+{
+    public class Employee<T1>
+    {
+        public T1 things;
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee<String> stringThing = new Employee<string>();
+            stringThing.things = "Things";
+
+            Employee<int> integerThing = new Employee<int>();
+            integerThing.things = 1;
+            foreach (var stringItem in stringThing)
+            {
+                Console.WriteLine(stringItem);
+            }
+            foreach (var intItem in integerThing)
+            {
+                Console.WriteLine(intItem);
+            }
+
+        }
+    }
+}
