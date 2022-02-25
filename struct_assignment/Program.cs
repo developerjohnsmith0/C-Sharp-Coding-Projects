8de76ea06e854011c3b363f4f0e90b734bd882d8
developerjohnsmith0 <87449450+developerjohnsmith0@users.noreply.github.com>
2022-02-25 09:49:31 -0500

    struct assignment

---
+++ struct_assignment/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace struct_assignment
+{
+    public struct Number
+    {
+        public decimal Amount;
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number amount = new Number();
+            amount.Amount = 150;
+            Console.WriteLine(amount.Amount);
+        }
+    }
+}
