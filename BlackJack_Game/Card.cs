24e6ded0d0634ac0e888ac7a3bc610f03486a71b
developerjohnsmith0 <87449450+developerjohnsmith0@users.noreply.github.com>
2022-02-25 09:51:37 -0500

    blackjack game

---
+++ BlackJack_Game/Card.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackJack_Game
+{
+    class Card
+    {
+        private string name;
+        private string suit;
+        private int rank;
+
+        public string Suit
+        {
+            get { return suit; }
+            set { suit = value; }
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public int Rank
+        {
+            get { return rank; }
+            set
+            {
+                if (value >= 12)
+                {
+                    rank = 10;
+                }
+                else
+                {
+                    rank = value;
+                }
+            }
+        }
+    }
+}
+
---
+++ BlackJack_Game/Deck.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackJack_Game
+{
+    class Deck
+    {
+        private List<Card> cardDeck = new List<Card>();
+
+        public List<Card> CardDeck { get => cardDeck; set => cardDeck = value; }
+
+        // Creates the playing deck.
+        public List<Card> BuildCardDeck()
+        {
+            Deck deck = new Deck();
+
+            foreach (MyEnums.Suit suit in Enum.GetValues(typeof(MyEnums.Suit)))
+            {
+                foreach (MyEnums.Rank rank in Enum.GetValues(typeof(MyEnums.Rank)))
+                {
+                    Card card = new Card();
+                    card.Suit = suit.ToString();
+
+                    // Set card name.
+                    if ((int)rank <= 10)
+                    {
+                        int temp = (int)rank;
+                        card.Name = temp.ToString();
+                    }
+                    else
+                    {
+                        card.Name = rank.ToString();
+                    }
+
+                    // Set card value.
+                    if ((int)rank > 11)
+                    {
+                        card.Rank = 10;
+                    }
+                    else
+                    {
+                        card.Rank = (int)rank;
+                    }
+
+                    deck.CardDeck.Add(card);
+                }
+            }
+
+            return deck.CardDeck;
+        }
+
+        // Shuffles the playing deck.
+        public List<Card> ShuffleDeck(List<Card> cardDeck)
+        {
+            var shuffledDeck = cardDeck.OrderBy(x => Guid.NewGuid()).ToList();
+
+            return shuffledDeck;
+        }
+    }
+}
